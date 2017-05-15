575ededd5b722d848418951c2add240e09363a2c
xuanhaoo <1335637386@qq.com>
2017-05-15 23:09:17 +0800

    提交student.php路由

--- app/Http/Routes/student.php
+++ app/Http/Routes/student.php
@@ -1,2 +1,7 @@
 <?php
-
+// 命名空间为‘Home’，url路由前缀为‘home’，
+Route::group(['prefix' => 'home','namespace'=>'Home'],function() {
+	Route::any('/login','LoginController@login');
+    Route::any('/checkLogin','LoginController@checkLogin');
+    Route::get('/testmenu','LoginController@testmenu');
+});
